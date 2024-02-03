c182ee7a86be4fa00b90d07e125ceb0eebcec04b
choiihwa <chldlghk@gmail.com>
2024-02-03 19:13:00 +0900

    STOMP관련 코드 주석 추가

--- src/main/java/com/ffs/chat/config/StompConfig.java
+++ src/main/java/com/ffs/chat/config/StompConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocketMessageBroker // WebSocket 활성화 어노테이션
 public class StompConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
@@ -19,7 +19,7 @@ public class StompConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/pub");
-        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub"); // 메시지 발행 요청
+        registry.enableSimpleBroker("/sub"); // 메시지 구독 요청
     }
 }
